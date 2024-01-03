b090481cfb15575f3c8cb33240cbb2a2c4a0e14f
Samuel Omoding <samuel.omoding@student.moringaschool.com>
2024-01-03 14:07:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-function fetchBooks() {
+async function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  const response = await
+  fetch('https://www.anapioficeandfire.com/api/books')
+     .then(response => response.json())
+     .then(data => {
+       const main = document.getElementById('book-titles');
+       data.forEach(book => {
+         const h2 = document.createElement('h2');
+         h2.textContent = book.name;
+         main.appendChild(h2);
+       });
+     })
 }
-
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+      fetchBooks();
+ 
